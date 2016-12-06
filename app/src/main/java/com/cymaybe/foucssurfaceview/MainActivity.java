87f426c3a1957d708fb7c7ae2e9728f7e80836a2
CGmaybe10 <409958786@qq.com>
2016-12-06 18:26:20 +0800

    添加拍照界面

--- app/src/main/java/com/cymaybe/foucssurfaceview/MainActivity.java
+++ app/src/main/java/com/cymaybe/foucssurfaceview/MainActivity.java
@@ -1,13 +1,239 @@
 package com.cymaybe.foucssurfaceview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.RectF;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.OrientationEventListener;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.cymaybe.foucsurfaceview.FocusSurfaceView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SurfaceHolder.Callback {
+    private static final String TAG = "moubiao";
+    public static final String TAKE_PICTURE = "picture";
+
+    private FocusSurfaceView previewSFV;
+    private Button mTakeBT;
+
+    private Camera mCamera;
+    private SurfaceHolder mHolder;
+    private boolean focus = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initData();
+        initView();
+        setListener();
+    }
+
+    private void initData() {
+        DetectScreenOrientation detectScreenOrientation = new DetectScreenOrientation(this);
+        detectScreenOrientation.enable();
+    }
+
+    private void initView() {
+        previewSFV = (FocusSurfaceView) findViewById(R.id.preview_sv);
+        mHolder = previewSFV.getHolder();
+        mHolder.addCallback(MainActivity.this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        mTakeBT = (Button) findViewById(R.id.take_bt);
+    }
+
+    private void setListener() {
+        mTakeBT.setOnClickListener(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        initCamera();
+        setCameraParams();
+    }
+
+    private void initCamera() {
+        try {
+            mCamera = android.hardware.Camera.open(0);//1:采集指纹的摄像头. 0:拍照的摄像头.
+            mCamera.setPreviewDisplay(mHolder);
+        } catch (Exception e) {
+            Snackbar.make(mTakeBT, "camera open failed!", Snackbar.LENGTH_SHORT).show();
+            finish();
+            e.printStackTrace();
+        }
+    }
+
+    private void setCameraParams() {
+        if (mCamera == null) {
+            return;
+        }
+        try {
+            Camera.Parameters parameters = mCamera.getParameters();
+
+            int orientation = judgeScreenOrientation();
+            if (Surface.ROTATION_0 == orientation) {
+                mCamera.setDisplayOrientation(90);
+                parameters.setRotation(90);
+            } else if (Surface.ROTATION_90 == orientation) {
+                mCamera.setDisplayOrientation(0);
+                parameters.setRotation(0);
+            } else if (Surface.ROTATION_180 == orientation) {
+                mCamera.setDisplayOrientation(180);
+                parameters.setRotation(180);
+            } else if (Surface.ROTATION_270 == orientation) {
+                mCamera.setDisplayOrientation(180);
+                parameters.setRotation(180);
+            }
+
+            //临时解决图像方向问题
+            mCamera.setDisplayOrientation(180);
+            parameters.setRotation(180);
+            parameters.setPictureSize(480, 270);//192 144  160 120 240 180 264 198 320 240
+            parameters.setPreviewSize(480, 270);
+            List<Camera.Size> supportedPreviewSizes = parameters.getSupportedPreviewSizes();
+            List<Camera.Size> sizes = parameters.getSupportedPictureSizes();
+            if (false) {
+                for (Camera.Size pre : supportedPreviewSizes) {
+                    Log.d(TAG, "setCameraParams: preview width = " + pre.width + " height = " + pre.height);
+                }
+
+                for (Camera.Size pic : sizes) {
+                    Log.d(TAG, "setCameraParams: pic width = " + pic.width + " height = " + pic.height);
+                }
+            }
+
+            mCamera.setParameters(parameters);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 判断屏幕方向
+     *
+     * @return 0：竖屏 1：左横屏 2：反向竖屏 3：右横屏
+     */
+    private int judgeScreenOrientation() {
+        return getWindowManager().getDefaultDisplay().getRotation();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        releaseCamera();
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.take_bt:
+                if (!focus) {
+                    takePicture();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 拍照
+     */
+    private void takePicture() {
+        mCamera.autoFocus(new Camera.AutoFocusCallback() {
+            @Override
+            public void onAutoFocus(boolean success, Camera camera) {
+                focus = success;
+                if (success) {
+                    mCamera.cancelAutoFocus();
+                    mCamera.takePicture(new Camera.ShutterCallback() {
+                        @Override
+                        public void onShutter() {
+                        }
+                    }, null, null, new Camera.PictureCallback() {
+                        @Override
+                        public void onPictureTaken(byte[] data, Camera camera) {
+                            savePicture(data);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void savePicture(byte[] data) {
+        //设置原始照片
+        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+        //裁剪框的位置和宽高
+        RectF frameRect = previewSFV.getFrameRect();
+        float frameLeft = frameRect.left;
+        float frameTop = frameRect.top;
+        float frameWidth = frameRect.width();
+        float frameHeight = frameRect.height();
+
+        //照片的宽高
+        Camera.Size picSize = mCamera.getParameters().getPictureSize();
+        float picWidth = picSize.width;
+        float picHeight = picSize.height;
+
+        //预览界面的宽高
+        float preWidth = previewSFV.getWidth();
+        float preHeight = previewSFV.getHeight();
+
+        //预览界面和照片的比例
+        float preRW = picWidth / preWidth;
+        float preRH = picHeight / preHeight;
+
+        int cropLeft = (int) (frameLeft * preRW);
+        int cropTop = (int) (frameTop * preRH);
+        int cropWidth = (int) (frameWidth * preRW);
+        int cropHeight = (int) (frameHeight * preRH);
+
+        Bitmap cropBmp = Bitmap.createBitmap(bitmap, cropLeft, cropTop, cropWidth, cropHeight);
+    }
+
+    /**
+     * 用来监测左横屏和右横屏切换时旋转摄像头的角度
+     */
+    private class DetectScreenOrientation extends OrientationEventListener {
+        DetectScreenOrientation(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void onOrientationChanged(int orientation) {
+            if (260 < orientation && orientation < 290) {
+                setCameraParams();
+            } else if (80 < orientation && orientation < 100) {
+                setCameraParams();
+            }
+        }
     }
 }
